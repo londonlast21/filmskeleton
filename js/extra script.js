68f637d70b4ad3167dba602bc9e13247472f2b78
jack <biffisbuffm@gmail.com>
2020-07-16 21:16:05 -0500

    change html and css

---
+++ js/extra script.js
@@ -0,0 +1,39 @@
+
+
+// api call to link to penguin api book database
+//var getBookImage = function() {
+
+    //console.log(easyInput);
+
+    // function to send api call with title 
+
+    // create image of poster and append to page
+    //var poster = document.createElement("p");
+    
+    // label src
+    
+
+    // append to page
+   // document.getElementById("#posterPic").appendChild('poster');
+   
+    
+//}
+
+
+// function to capture the input from the searchBar
+// var getInput = function(event) {
+//     //set input to the value of text entered
+//     var input = document.getElementById('searchBar').value;
+//     // make entered text all lowercase
+//     var easyInput = input.toLowerCase();
+    
+//     // test what value is being used
+//     console.log(easyInput);
+
+//     //redefine what getInput is to use in api calls
+//     var getInput = easyInput;
+
+    
+    
+// };
+
--- js/script.js
+++ js/script.js
@@ -1,22 +1,33 @@
-// api call to link to penguin api book database
-var getBookImage = function() {
-    
-   
-    
-}
+// capture input from getInput onclick function
 
+// use captured information (title) to define a variable to send to api
 
-// function to capture the input from the searchBar
-var getInput = function(event) {
-    //set input to the value of text entered
-    var input = document.getElementById('searchBar').value;
-    // make entered text all lowercase
-    var easyInput = input.toLowerCase();
-    
-    // test what value is being used
-    console.log(easyInput);
+// send to film poster api to retrieve image
 
-    // send entered value to the api call
-    getBookImage();
-};
+// generate poster image on page
+
+// send captured info (title) to imdb to retrieve rating
+
+// have dom dynamically generate box indicating rating
+
+// have dom send title to 
+
+// make entire appended object movable
+
+// make it possible to append drag object to either section
+
+// use localStorage to set the value [i] for each object as an object of
+// 'seenTitle[i]' and 'notSeenTitle[i]'
+
+---
+user creates custom dashboard usin existing icons wh are given 
+mental health diagnosis values and media searched images from multiple apis that generate image results
+
+user can move items between columns; dashboard is saved to local Storage
+
+user can drag a trigger icon to help resources GamepadButton
+resources button sends logged issue to api to return 5 specialists/resources
+
+clicking resource leads you to relevant Info
+
 
