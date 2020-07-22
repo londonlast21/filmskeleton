34f7c733bbc3ab4df63bde54619202243dd68d3b
jack <biffisbuffm@gmail.com>
2020-07-22 13:03:53 -0500

    fixed all spacing issues

--- js/extra script.js
+++ js/extra script.js
@@ -1,39 +1,52 @@
+// capture input from getInput onclick function
 
+// use captured information (title) to define a variable to send to api
 
-// api call to link to penguin api book database
-//var getBookImage = function() {
+// send to film poster api to retrieve image
 
-    //console.log(easyInput);
+// generate poster image on page
 
-    // function to send api call with title 
+// send captured info (title) to imdb to retrieve rating
 
-    // create image of poster and append to page
-    //var poster = document.createElement("p");
-    
-    // label src
-    
+// have dom dynamically generate box indicating rating
 
-    // append to page
-   // document.getElementById("#posterPic").appendChild('poster');
-   
-    
-//}
+// have dom send title to 
 
+// make entire appended object movable
 
-// function to capture the input from the searchBar
-// var getInput = function(event) {
-//     //set input to the value of text entered
-//     var input = document.getElementById('searchBar').value;
-//     // make entered text all lowercase
-//     var easyInput = input.toLowerCase();
-    
-//     // test what value is being used
-//     console.log(easyInput);
+// make it possible to append drag object to either section
 
-//     //redefine what getInput is to use in api calls
-//     var getInput = easyInput;
+// use localStorage to set the value [i] for each object as an object of
+// 'seenTitle[i]' and 'notSeenTitle[i]'
 
-    
-    
-// };
+// ---
+// user creates custom dashboard usin existing icons wh are given 
+// mental health diagnosis values and media searched images from multiple apis that generate image results
+
+// user can move items between columns; dashboard is saved to local Storage
+
+// user can drag a trigger icon to help resources GamepadButton
+// resources button sends logged issue to api to return 5 specialists/resources
+
+// clicking resource leads you to relevant Info
+
+// start of icon-value pairing section //
+// mood related icons
+// icons with an id of "depression" function: cloud, sad-face, happiness:happiness,
+// anger: anger-fire,
+
+/
+
+// <!-- addiction-related icons ids-->
+// tech-addiction(android, games )addiction-alcoholism: drink-hard, smoking,
+
+// <!-- mental disorder icons ids-->
+// paranoia, eating-disorders, 
+
+// <!-- life-related icons ids-->
+// disability, money-management:credit-card,boundaries: boundaries, exercise: running,
+// sleep: sleep, parenting: parenting,
+
+// if i give all icons links: is it possible to run their links ONLY on dragging them to the magnifying glass?
+// or if not: run the href link to for whatever i give that variable as a link in their category?
 
--- js/script.js
+++ js/script.js
@@ -1,51 +1,13 @@
-// capture input from getInput onclick function
 
-// use captured information (title) to define a variable to send to api
+// function to capture the input from the searchBar
+var getInput = function(event) {
+    //set input to the value of text entered
+    var input = document.getElementById('searchBar').value;
+    // make entered text all lowercase
+    var easyInput = input.toLowerCase();
+    
+    // test what value is being used
+    console.log(easyInput);
 
-// send to film poster api to retrieve image
-
-// generate poster image on page
-
-// send captured info (title) to imdb to retrieve rating
-
-// have dom dynamically generate box indicating rating
-
-// have dom send title to 
-
-// make entire appended object movable
-
-// make it possible to append drag object to either section
-
-// use localStorage to set the value [i] for each object as an object of
-// 'seenTitle[i]' and 'notSeenTitle[i]'
-
-// ---
-// user creates custom dashboard usin existing icons wh are given 
-// mental health diagnosis values and media searched images from multiple apis that generate image results
-
-// user can move items between columns; dashboard is saved to local Storage
-
-// user can drag a trigger icon to help resources GamepadButton
-// resources button sends logged issue to api to return 5 specialists/resources
-
-// clicking resource leads you to relevant Info
-
-// start of icon-value pairing section //
-// mood related icons
-// icons with an id of "depression" function: cloud, sad-face, happiness:happiness,
-// anger: anger-fire,
-
-/
-
-// <!-- addiction-related icons ids-->
-// tech-addiction(android, games )addiction-alcoholism: drink-hard, smoking,
-
-// <!-- mental disorder icons ids-->
-// paranoia, eating-disorders, 
-
-// <!-- life-related icons ids-->
-// disability, money-management:credit-card,boundaries: boundaries, exercise: running,
-// sleep: sleep, parenting: parenting,
-
-// if i give all icons links: is it possible to run their links ONLY on dragging them to the magnifying glass?
-// or if not: run the href link to for whatever i give that variable as a link in their category?
+    // send input to api call
+};
