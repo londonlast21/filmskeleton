a733db47027eaa9d33372d165ea4182736fa5aac
jack <biffisbuffm@gmail.com>
2020-07-22 16:07:56 -0500

    worked on spacing

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Framework by Skeleton
 Icons by GoogleIcons
 
 ## API Used
+"This product uses the TMDb API but is not endorsed or certified by TMDb." needs official logo as well
--- js/script.js
+++ js/script.js
@@ -1,3 +1,48 @@
+// // api call to generate image
+// var getMedia = function(){
+
+// };
+
+// // function to create note card based on user input
+// if mediaType = film {
+
+// }
+
+// // function
+// else if mediaType = book {
+
+// }
+// // function
+
+// else if mediaType = general {
+
+// }
+
+// else {
+//     // generate prompt 'select category'
+    
+
+// }
+
+//--------------//
+
+// function to create note based on user note input
+
+
+
+
+// function to run an icon's html on drag to anchor
+
+
+
+
+// function to set localStorage for working on column
+// function to set localStorage for trigger column
+// function to set localStorage for weekly thing
+// function to restore localStorage for all on page refresh
+
+
+// var to get media input
 var getTitleInfo = function(event) {
     var input = document.getElementById('titleSearch').value;
     // make entered text all lowercase
@@ -12,13 +57,36 @@ var getTitleInfo = function(event) {
     console.log(mediaType);
 
     // clear the text input
-    document.getElementById("titleSearch").value = "";
+    document.getElementById('titleSearch').value = "";
 
     // send these values to the api calls
+    //getMedia(easyInput, mediaType);
+}
+
+// var to get note input
+var getNoteInfo = function(event) {
+
+    // capture input from textarea
+    var getNote = document.getElementById('new-note').value;
+    // test what was captured
+    console.log(getNote);
+
+
+    // clear note input area on submission
+    var noteInput = document.getElementById('new-note').value = "";
+    
+    // use note input to generate note card
+
+
 }
 
 
 /// onclick to get data user's input from script in html tag
 document.getElementById("getTitle").onclick = function (event){
     getTitleInfo();     
+}
+
+// onclick to get data from user's input in week div
+document.getElementById("save-new-event").onclick = function (event){
+    getNoteInfo();
 }
