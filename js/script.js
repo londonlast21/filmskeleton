e281b3323262c4df2d4aa67f34d497f5a788f275
jack <biffisbuffm@gmail.com>
2020-07-22 18:14:10 -0500

    api calls fixed

--- js/script.js
+++ js/script.js
@@ -1,24 +1,60 @@
 var easyInput = "";
 var mediaType ="";
+var apikey = "b8918a77ef3da9c62a4dc045d9aa28ee";
 
 
 
 // api call to generate image
-var getMedia = new function(){
+var getMedia = function(easyInput, mediaType){
 
-    // use the user input values of mediaType & easyInput
+    // clear old results
+    document.getElementById('titleImage').innerHTML = "";
 
-    if mediaType === film {
     
-    // send input to get JSON response
-    GET https://api.themoviedb.org/3/movie//images?api_key= + apikey + &language=en-US
+
+    // if media type value is 'film'
+    if (mediaType === "Film") {
+
+    // send input to tmdb api call to get JSON response
+    var url = "https://api.themoviedb.org/3/search/movie?api_key="+ apikey + "&query=" + easyInput;
+
+    fetch (url)
+    .then(response => response.json())
+    .then(function(data) {
+        console.log(data);
+
+        // function to create note card based on user input
+        var movie = document.createElement('img');
+        movie.className = "titleCard";
+        movie.setAttribute("src", "http://image.tmdb.org/t/p/w185/"+ data.results[0].poster_path)
+        document.getElementById('titleImage').appendChild(movie);
 
 
-    // function to create note card based on user input
-    var movie[i] = document.createElement('img');
-    movie[i].className = "titleCard";
-    document.getElementById('titleImage').appendChild(img);
+    });}
+    else if (mediaType === "Book"){
+    
+        var url = "https://www.googleapis.com/books/v1/volumes?q=" + easyInput;
 
+        fetch (url)
+        .then(response => response.json())
+        .then(function(data) {
+            console.log(data);
+
+            // function
+            var book = document.createElement('img');
+            book.className = "titleCard";
+            book.setAttribute("src", data.items[0].volumeInfo.imageLinks.thumbnail);
+            document.getElementById('titleImage').appendChild(book);
+        })
+
+    } else if (mediaType === "General"){
+
+
+  
+
+
+
+    
     }
     // // if book is selected, run library API call
     // else if mediaType === book {
@@ -54,10 +90,6 @@ var getMedia = new function(){
 
 
 // function to run an icon's html on drag to anchor
-
-
-
-
 
 
 // function to set localStorage for working on column
@@ -109,7 +141,6 @@ var getNoteInfo = function(event) {
 document.getElementById("getTitle").onclick = function (event){
     getTitleInfo();     
 }
-
 // onclick to get data from user's input in week div
 document.getElementById("save-new-event").onclick = function (event){
     getNoteInfo();
