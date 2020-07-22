8402632e571d9e27ea7cde144175c6296d4672dd
jack <biffisbuffm@gmail.com>
2020-07-22 14:18:34 -0500

    onclick

--- js/script.js
+++ js/script.js
@@ -1,22 +1,5 @@
 
-// function to capture the input from the searchBar
-// when onClick
-function getInput = function(event) {
-    //set input to the value of text entered
-    var input = document.getElementById('searchBar').value;
-    // make entered text all lowercase
-    var easyInput = input.toLowerCase();
-    
-    // test what value is being used
-    console.log(easyInput);
-
-    // capture the type of category selected
-    var dropDown = document.getElementById('dropdown-bar');
-    var mediaType = dropDown.options[dropDown.selectedIndex].text;
-
-    // test what value is being used
-    console.log(mediaType);
-
-    // send easyInput and mediaType to api call
-
-};
+/// onclick to get data user's input from script in html tag
+document.getElementById("getInput").onclick = function (event){
+    console.log("hello");
+}
