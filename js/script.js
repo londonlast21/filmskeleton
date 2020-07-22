c521fe3d13e1b61bf08b5580c2d9ca725e144a49
jack <biffisbuffm@gmail.com>
2020-07-22 14:45:13 -0500

    capture values and clear text entry field

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
+var getTitleInfo = function(event) {
+    var input = document.getElementById('titleSearch').value;
+    // make entered text all lowercase
+    var easyInput = input.toLowerCase();
+    //test what is being used
+    console.log(easyInput);
+
+    // capture type of category selected
+    var dropDown = document.getElementById('dropdown-bar');
+    var mediaType = dropDown.options[dropDown.selectedIndex].text;
+    // test what value is being used
+    console.log(mediaType);
+
+    // clear the text input
+    document.getElementById("titleSearch").value = "";
+
+    // send these values to the api calls
+}
+
 
 /// onclick to get data user's input from script in html tag
-document.getElementById("getInput").onclick = function (event){
-    console.log("hello");
+document.getElementById("getTitle").onclick = function (event){
+    getTitleInfo();     
 }
